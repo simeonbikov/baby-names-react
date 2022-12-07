0ce7a7bdb0e696b9078fa6b63ac56961884763fc
simeonbikov <bikovsem@gmail.com>
2022-12-07 14:24:22 +0000

    fixed input search

--- src/App.js
+++ src/App.js
@@ -6,41 +6,26 @@ import "./App.css";
 
 function App() {
   const [targetName, setTargetName] = useState("");
+  let allNames = [...babyNamesData];
 
+  // const makeListForMatchingNames = (query) =>
+  //   babyNamesData.filter((baby) => contains(baby.name, query));
 
-    const makeListForMatchingNames = (query) =>
-      babyNamesData.filter((baby) => contains(baby.name, query));
-    // const filteredNames = babyNamesData.filter((baby) => contains(baby.name, query));
-    // console.log(filteredNames);
-    // makePageForShows(filtered);
-    // return filtered;
-    // };
+  const makeListForMatchingNames = (query) => {
+    if (query) {
+      return allNames.filter((baby) => contains(baby.name, query));
+    } else {
+      return allNames;
+    }
+  };
 
-    const contains = (inspectStr, targetStr) => {
-      return (
-        inspectStr &&
-        targetStr &&
-        -1 !== inspectStr.toLowerCase().indexOf(targetStr.toLowerCase())
-      );
-    };
-
-  // let filteredNames = babyNamesData;
-
-  // const SearchInput = () => {
-    
-  //   const handleInputChange = (event) => {
-  //     setTargetName(event.target.value);
-  //     makeListForMatchingNames(event.target.value); //
-  //   };
-  //   return (
-  //     <input
-  //       type="text"
-  //       placeholder="Search name ..."
-  //       value={targetName}
-  //       onChange={handleInputChange}
-  //     />
-  //   );
-  // };
+  const contains = (inspectStr, targetStr) => {
+    return (
+      inspectStr &&
+      targetStr &&
+      -1 !== inspectStr.toLowerCase().indexOf(targetStr.toLowerCase())
+    );
+  };
 
   const handleInputChange = (event) => {
     let filteredNames = makeListForMatchingNames(event.target.value); //
@@ -53,7 +38,7 @@ function App() {
       <div className="container">
         <SearchInput handleChange={handleInputChange} />
         {/* <ListOfNames list={targetName} /> */}
-        <ListOfNames list={targetName ? targetName : babyNamesData} />
+        <ListOfNames list={targetName ? targetName : allNames} />
       </div>
     </div>
   );
